b53776ff03aa29e642eda086700113131ba0a5e4
Misha Lebedev <555Leprikon555@gmail.com>
2019-02-10 22:23:02 +0200

    String Slices commit 10.02.19

--- String/String Slices.py
+++ String/String Slices.py
@@ -1,9 +1,7 @@
-string = "Hello Python"
-print(string)
-print(string[0])
-print(string[1])
-print(string[0:5])
-print(string[6:12])
-print(string[6:])
-print(string[:5])
-print(string[5])
+line = "Hello Python"
+
+line_characters = '\n''\n''\n'.join(line) # output each character individually
+every_second_letter = '\n''\n''\n'.join(line[::2]) # output of every second character of a line
+return_line = '\n''\n''\n'.join(line[::-1]) # character string output in reverse order
+
+print(line_characters,'\n''\n''\n', every_second_letter, '\n''\n''\n', return_line)
