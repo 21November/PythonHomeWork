e3cb46c3f00a0e74a71d8327afff9c4c27259ffb
Nikita <niita.limarchuk@gmail.com>
2019-02-19 22:55:48 +0200

    added codestyle xml configuration file added comments for homework 3

--- lesson_3/homework_3.py
+++ lesson_3/homework_3.py
@@ -40,3 +40,32 @@ def selection_sort(arrayToSort):
     return a
 
 print(selection_sort(list_5))
+# region comments for homework 3 tasks 1-3
+# TODO: common issue - You should numerate each sub task with some comment
+# TODO: common issue - You should wrap blocks of functional code into regions. It will allow you to expand and
+#                      collapse it into lists.
+# TODO: common issue - Hard to understand all this prints. Please add description for each print. For example "Not
+#                      sorted list", "Sorted list"
+# TODO: common issue - before next push try to sue code analyzer > Code -> Inspect Code -> Whole project -> OK.
+#                      Remove all warnings issues and errors from iteration to iteration until no errors left.
+# TODO: line 4 - make space after comma
+# TODO: line 7 - customization of str to str make no sense
+# TODO: line 9 - best practice to use one code style. In python no difference between such two strings 'Python' and
+#                "Python". Try to use single quotes for internal strings or for separated characters ("my outer
+#                string 'my inner string'", "Simple string", '\n', 'n').
+# TODO: lines 16-17 - please write difference between both methods. Try to print result of this functions to see
+#                     difference.
+# TODO: line 19 - you print almost same lists as in task 1. Please create 1 set of variables for work but with noto
+#                 so confusing names.
+# TODO: line 21 - no space after # before comment text
+# TODO: line 30 - 2 emtpy lines should be before function declaration.
+# TODO: line 33 - all variables and attributes should be lower case with underscore
+# TODO: line 36 - all variables and attributes should be lower case with underscore
+# TODO: line 31 - why do you create another one internal variable of array if you can use array from arguments?
+# TODO: line 31 - variable name is confusing and not clear destination of such variable
+# TODO: line 32 - variable name is confusing and not clear destination of such variable
+# TODO: line 34 - variable name is confusing and not clear destination of such variable
+# TODO: line 37 - variable name is confusing and not clear destination of such variable
+# TODO: lines 31 - 40 - no comments. Please add some comments about work of your algorithm.
+# TODO: line 42 - no empty line after code
+# endregion
