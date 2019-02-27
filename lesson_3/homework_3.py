7973bead0212b213083c16ea31ad4b16a966efd0
Misha Lebedev <555Leprikon555@gmail.com>
2019-02-27 22:03:39 +0200

    In Homework_3 added sub-items to tasks: Task 1.1 - Create a list Task 1.2 - Create a list using list constructor Task 1.3.1 - Create a list of strings Task 1.3.2 - Create a list using the split method Task 1.4 - Create list using list comprehension Task 2.1 - Add item to the end of the Regular_list Task 2.2 - Add item to List_constructor at fixed index Task 2.3 - Remove item from Regular_list at fixed index Task 2.4 - The function removes a specific Regular_list item Task 2.5 - The function 'pop' deletes a list item by its index Task 3.1 - Sorting the list using the function 'reverse' and 'sort' Task 3.2 - Ascending sorting algorithm Task 4.1 - Filter numbers >0 using Filter function with lambda Task 4.2 - Filter numbers >0 using Filter function with callback Task 4.3 - Filter number >0 using list comprehension Task 4.4 - Create copy of link for this list Task 4.5 - Create copy of list using module copy.deepcopy Task 4.6 - Create copy of list using list slices Task 4.7 - Multiply all elements of the The_sun list by 5 Task 4.8 - Combine all elements of the list into a string Task 4.11 - Change every list item Task 4.12 - Change each element of the list using the lambda function Task 4.13 - Find the minimum value of the list using the standard function Task 4.14 - Find the maximum value of the list using the standard function Task 4.15 - The algorithm for finding the maximum and minimum values ​​of the list

--- lesson_3/homework_3.py
+++ lesson_3/homework_3.py
@@ -1,47 +1,146 @@
+import copy
+from random import randint
+
 # Task 1:
 # Task 1.1:
-list_1 = [1, 2, 3, 4, 5]
+Regular_list = [1, 2, 3, 4, 5]                      # Create a list
+print('Task 1.1:', Regular_list)
+
 # Task 1.2:
-list_2 = [x**2 for x in [1,2,3,4]]
-text_1 = "Python"
-list_3 = list(text_1)
+List_constructor = [x**2 for x in [1,2,3,4]]        # Create a list using list constructor
+print('Task 1.2:', List_constructor)
 
-list_4 = [str(x) for x in text_1]
+# Task 1.3.1:
+text = "Python is a whole world!!!"
+List_string = list(text)                            # Create a list of strings
+print('Task 1.3.1:', List_string)
 
-print('Task 1:', list_1, list_2, list_3, list_4, sep='\n')
+# Task 1.3.2:
+print('Task 1.3.2:', text.split( ))                 # Create a list using the split method
+
+# Task 1.4:
+List_for = [str(x) for x in text]                   # Create list using list comprehension
+print('Task 1.4:', List_for)
 
 # Task 2:
+# Task 2.1:
+Regular_list.append(6)                              # Add item to the end of the Regular_list
+print('Task 2.1:', Regular_list)
 
-list_1.append(6)         # Add item to the end of the list_1
-list_2.insert(1, 10)     # Add item to list_2 at fixed index
-del list_3[-1]           # Remove item from list_3 at fixed index
-list_1.remove(3)         # The function removes a specific list_1 item.
-list_2.pop(0)            # The function 'pop' deletes a list item by its index
+# Task 2.2:
+List_constructor.insert(1, 99)                      # Add item to List_constructor at fixed index
+print('Task 2.2:', List_constructor)
 
-print('Task 2:', list_1, list_2, list_3[-1], list_1[3], list_3, list_1, list_2, sep='\n')
+# Task 2.3:
+del Regular_list[-1]                                 # Remove item from Regular_list at fixed index
+print('Task 2.3:', Regular_list[-1])
+
+# Task 2.4:
+Regular_list.remove(3)                              # The function removes a specific Regular_list item.
+print('Task 2.4:', Regular_list[3])
+
+# Task 2.5:
+List_constructor.pop(0)                             # The function 'pop' deletes a list item by its index
+print('Task 2.5:', List_constructor)
 
 #Task 3:
+# Task 3.1:
+apple = [21, 10, 18, 5, 15, 4, 2, 1, 30]
+apple.sort()                                        # sorting by increment list using function 'sort'
+plum = [21, 10, 18, 5, 15, 4, 2, 1, 30]
+plum.reverse()                                     # sorting the list in descending order using the function 'reverse'
 
-list_5 = [21, 10, 18, 5, 15, 4, 2, 1, 30]
-list_5.sort()                                 # sorting by increment list using function 'sort'
-list_6 = [21, 10, 18, 5, 15, 4, 2, 1, 30]
-list_6.reverse()                              # sorting the list in descending order using the function 'reverse'
+print('Task 3.1:', apple, plum, sep='\n')
 
-print('Task 3:', list_5, list_6, sep='\n')
+# Task 3.2: Ascending sorting algorithm
+# create a list
+n = 10                                             # list length
+pear = []
+for i in range(n):
+    pear.append(randint(1, 99))                    # use a random number generator
+print('Task 3.2: Unordered list',pear)
 
-def selection_sort(arrayToSort):
-    a = arrayToSort
-    for i in range(len(a)):
-        idxMin = i
-        for j in range(i+1, len(a)):
-            if a[j] < a[idxMin]:
-                idxMin = j
-        tmp = a[idxMin]
-        a[idxMin] = a[i]
-        a[i] = tmp
-    return a
+def sort_function(c):
+    for j in range(0,c - 1):                           # outer loop counts the number of "passes" in the list
+        for a in range(0,c - j -1):                     # nested cycle compares i-th and i + 1-th element and,
+                                                     # if necessary, changes their places the number of
+                                                     # comparisons each time decreases by j
+            if pear[a] > pear[a+1]:
+                pear[a],pear[a + 1] = pear[a + 1], pear[a]
+print('Task 3.2: Sorted list', pear)
 
-print(selection_sort(list_5))
+# Task 4:
+# Task 4.1:
+The_sun = [-1, 2, 0, 10, 3, 4, -5, -10, 8, 1, 3, -2, 12, -4, -8, 9]     # list with numbers not ordered
+Mercury = list (filter(lambda x : x > 0, The_sun))                      # Filter numbers > 0 using Filter function
+                                                                        # with lambda
+print("Task 4.1:", Mercury)
+
+# Task 4.2:
+Venus = list (filter(lambda x: isinstance(x, int) and x > 0, The_sun))  # Filter numbers > 0 using Filter function
+                                                                        # with callback
+print("Task 4.2:",Venus)
+
+# Task 4.3:
+Earth = [x for x in The_sun if x > 0]                                   # Filter number > 0 using list comprehension.
+print("Task 4.3:", Earth)
+
+# Task 4.4:
+The_sun2 = The_sun                                                      # Create copy of link for this list.
+print("Task 4.4:", The_sun2)
+
+# Task 4.5:
+The_sun3 = copy.deepcopy(The_sun)                                       # Create copy of list using module copy.deepcopy
+print("Task 4.5:", The_sun3)
+
+# Task 4.6:
+The_sun4 = The_sun.copy()                                               # Create copy of list using list slices.
+print("Task 4.6:", The_sun4)
+
+# Task 4.7:
+Mars = [x * 5 for x in The_sun]                                         # Multiply all elements of the The_sun list by 5
+print("Task 4.7:", Mars)
+
+# Task 4.8:
+Jupiter = ','.join(map(str, The_sun))                                   # Combine all elements of the list into a string
+print("Task 4.8:", Jupiter)
+
+# Task 4.9:
+# for item in The_sun:print ("Task 4.9:",item)
+
+# Task 4.11:
+Saturn = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Titanium = [i*2 for i in Saturn]                  # Change every list item
+
+print("Task 4.11:", Titanium)
+
+# Task 4.12:
+Ray = list(map(lambda x: x * 3, Saturn))    # Change each element of the list using the lambda function
+print("Task 4.12:", Ray)
+
+# Task 4.13:
+Uranus = [10, 15, 20, 5, 0, -20, -5, -10, 25]
+Titania = min(Uranus)                             # Find the minimum value of the list using the standard function
+print("Task 4.13:", Titania)
+
+# Task 4.14:
+Oberon = max(Uranus)                               # Find the maximum value of the list using the standard function
+print("Task 4.14:", Oberon)
+
+# Task 4.15 - 4.16:
+Neptune = [10, 15, 20, 5, 0, -20, -5, -10, 25]
+def minmax1 (x):
+    minimum = maximum = x[0]
+    for b in x[1:]:
+        if b < minimum:
+            minimum = b
+        else:
+            if b > maximum: maximum = b
+    return minimum, maximum
+print("Task 4.15 - 4.16:",minmax1(Neptune))
+
+
+
 # region comments for homework 3 tasks 1-3
 # TODO: common issue - You should numerate each sub task with some comment
 # TODO: common issue - You should wrap blocks of functional code into regions. It will allow you to expand and
